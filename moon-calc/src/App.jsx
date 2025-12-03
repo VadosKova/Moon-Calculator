444b2470e83ce0f3e6cd7c0d612812216ffa17da
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-12-03 18:05:42 +0200

    Imported icons with necessary hooks

--- moon-calc/src/App.jsx
+++ moon-calc/src/App.jsx
@@ -1,4 +1,9 @@
 import './App.css'
+import { useState, useEffect } from "react";
+import LogoImage from './assets/logo.png'
+import CopyImage from './assets/copy.png'
+import SaveImage from './assets/save.png'
+import WidgetImage from './assets/widget.png'
 
 function App() {
 
